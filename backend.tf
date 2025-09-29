d6fb9f9c120192980095bcf5be8d7af84c95063f
Prani2018 <46190678+Prani2018@users.noreply.github.com>
2025-09-28 20:02:41 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     # Specify the name of the S3 bucket to store the state file
     bucket = "jenkins-terraform-kubernetes-app-2024-v2"
     # Specify the AWS region where the bucket is located
-    region = "ap-south-1"
+    region = "us-east-1"
     # Specify the path within the bucket to store the state file
     key = "jenkins-server/terraform.tfstate"
   }
